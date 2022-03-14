7f9ee19b075d59d33d3d5b458d00a9129717fb55
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-14 18:05:08 +0100

    stesura finale

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,9 @@
 for( i=1; i<=100; i++ ) {
-    if( i % 3 == 0  ){
-        console.log (`Il ${i} è divisibile per 3`)
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`Il ${i} è divisibile per 3 e per 5`)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 
-        <div class="py-5 bg-box1 bg-fizz">Fizz</div> 
+        <div class="py-5 bg-box1 bg-fizzbuzz">FizzBuzz</div> 
         </div>
         `
     }
@@ -17,15 +17,16 @@ for( i=1; i<=100; i++ ) {
         `
     }
     
-    else if (i % 3 == 0 && i % 5 == 0) {
-        console.log(`Il ${i} è divisibile per 3 e per 5`)
+    
+    else if( i % 3 == 0  ){
+        console.log (`Il ${i} è divisibile per 3`)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 
-        <div class="py-5 bg-box1 bg-fizzbuzz">FizzBuzz</div> 
+        <div class="py-5 bg-box1 bg-fizz">Fizz</div> 
         </div>
         `
     }
-    else (i <= 100){
+    else {
         console.log (i)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 

80ed44edc730d9b7e8d594202467134c9dce7580
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-14 17:58:24 +0100

    prova

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,30 +1,35 @@
 for( i=1; i<=100; i++ ) {
-    document.getElementById("contenitoreNumeri").innerHTML += `
-    <div class="col_custom text-center fs-2 p-3"> 
-        <div class="py-5 bg-box1">${i}</div> 
-    </div>
-    `
     if( i % 3 == 0  ){
+        console.log (`Il ${i} è divisibile per 3`)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 
-            <div class="py-5 bg-box1 bg-fizz">Fizz</div> 
+        <div class="py-5 bg-box1 bg-fizz">Fizz</div> 
         </div>
         `
     }
-
-    if( i % 5 == 0  ){
+    
+    else if( i % 5 == 0  ){
+        console.log (`Il ${i} è divisibile per 5`)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 
-            <div class="py-5 bg-box1 bg-buzz">Buzz</div> 
+        <div class="py-5 bg-box1 bg-buzz">Buzz</div> 
         </div>
         `
     }
-
-    if (i % 3 == 0 && i % 5 == 0) {
+    
+    else if (i % 3 == 0 && i % 5 == 0) {
         console.log(`Il ${i} è divisibile per 3 e per 5`)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 
-            <div class="py-5 bg-box1 bg-fizzbuzz">FizzBuzz</div> 
+        <div class="py-5 bg-box1 bg-fizzbuzz">FizzBuzz</div> 
+        </div>
+        `
+    }
+    else (i <= 100){
+        console.log (i)
+        document.getElementById("contenitoreNumeri").innerHTML += `
+        <div class="col_custom text-center fs-2 p-3"> 
+            <div class="py-5 bg-box1">${i}</div> 
         </div>
         `
     }

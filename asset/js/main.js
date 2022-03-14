c7b46aae6d3a8c4375976493835eb68a6a57cf49
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-14 13:13:57 +0100

    stesura esercizio

---
+++ asset/js/main.js
@@ -0,0 +1,30 @@
+for( i=1; i<=100; i++ ) {
+    document.getElementById("contenitoreNumeri").innerHTML += `
+    <div class="col_custom text-center fs-2 p-3"> 
+        <div class="py-5 bg-box1">${i}</div> 
+    </div>
+    `
+    if( i % 3 == 0  ){
+        document.getElementById("contenitoreNumeri").innerHTML += `
+        <div class="col_custom text-center fs-2 p-3"> 
+            <div class="py-5 bg-box1 bg-fizz">Fizz</div> 
+        </div>
+        `
+    }
+
+    if( i % 5 == 0  ){
+        document.getElementById("contenitoreNumeri").innerHTML += `
+        <div class="col_custom text-center fs-2 p-3"> 
+            <div class="py-5 bg-box1 bg-buzz">Buzz</div> 
+        </div>
+        `
+    }
+
+    if (i % 3 == 0 && i % 3 == 0) {
+        document.getElementById("contenitoreNumeri").innerHTML += `
+        <div class="col_custom text-center fs-2 p-3"> 
+            <div class="py-5 bg-box1 bg-fizzbuzz">FizzBuzz</div> 
+        </div>
+        `
+    }
+}

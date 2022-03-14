24ee8425be0bb4d0ac04227350b36cfad910528a
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-14 13:16:44 +0100

    leggera modifica

--- asset/js/main.js
+++ asset/js/main.js
@@ -20,7 +20,8 @@ for( i=1; i<=100; i++ ) {
         `
     }
 
-    if (i % 3 == 0 && i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`Il ${i} è divisibile per 3 e per 5`)
         document.getElementById("contenitoreNumeri").innerHTML += `
         <div class="col_custom text-center fs-2 p-3"> 
             <div class="py-5 bg-box1 bg-fizzbuzz">FizzBuzz</div> 
